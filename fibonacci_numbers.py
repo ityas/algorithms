84e0f64a2d2350e2bc2dfa9f0145c6d5b6476b0f
ItYaS Woker <70561906+ItYaS@users.noreply.github.com>
2021-03-09 21:01:18 +0700

    Add files via upload

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -1,4 +1,14 @@
-# возвращает список с числами Фибоначчи
+# возвращает n-ое число Фибоначчи
+def fibonacci_num(n):
+    first_num = 0
+    last_num = 1
+    for _ in range(1, n):
+        first_num, last_num = last_num, first_num + last_num
+
+    return last_num
+
+
+# возвращает список с числами Фибоначчи до limit
 def fibonacci_list(limit):
     fib_nums = [0, 1]
     while len(fib_nums) <= limit:  # наполнение по условию
@@ -6,15 +16,3 @@ def fibonacci_list(limit):
         fib_nums.append(next_num)
 
     return fib_nums
-
-
-# возвращает n-ое число Фибоначчи
-def fibonacci_num(n):
-    first_num = 0
-    last_num = 1
-    for _ in range(1, n):
-        num = first_num + last_num
-        first_num = last_num
-        last_num = num
-
-    return last_num

656f80f2a27b11370bd03e2359a6782f1ac23b0c
ItYaS Woker <70561906+ItYaS@users.noreply.github.com>
2021-04-08 20:48:30 +0700

    Delete euclid_algorithm.py

--- others/euclid_algorithm.py
+++
@@ -1,8 +0,0 @@
-# алгоритм Евклида
-def euclid_algorithm(a, b):
-    if a == 0 or b == 0:
-        return max(a, b)
-    elif a >= b:
-        return euclid_algorithm(a % b, b)
-    elif b >= a:
-        return euclid_algorithm(a, b % a)

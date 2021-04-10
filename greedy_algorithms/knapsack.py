83e5f67c92ecc8ee928e03c1e2eb61522ad9b333
ItYaS Woker <70561906+ItYaS@users.noreply.github.com>
2021-04-10 13:47:15 +0700

    Add files via upload

--- greedy_algorithms/knapsack.py
+++ greedy_algorithms/knapsack.py
@@ -1,6 +1,7 @@
+# things = [[price, weight], [price, weight]]
+
 # решение задачи о рюкзаке(c делением вещей на части)
 def knapsack_with_proportion(things, knapsack_weight):
-    # result_list = []
     things.sort(key=lambda l: l[0] / l[1], reverse=True)  # сортировка по c/w
 
     price = 0
@@ -11,8 +12,6 @@ def knapsack_with_proportion(things, knapsack_weight):
 
         future_weight = thing[1] + weight
         if future_weight <= knapsack_weight:
-            # result_list.append([thing[0], thing[1]])
-
             price += thing[0]
             weight += thing[1]
         elif future_weight > knapsack_weight:
@@ -21,15 +20,15 @@ def knapsack_with_proportion(things, knapsack_weight):
 
             price += proportion_price
             weight += knapsack_weight - weight
-
-            # result_list.append([proportion_price, thing[1]])
             break
 
-    return price  # , result_list
+    return price
 
 
 # решение задачи о рюкзаке(c повторением вещей)
 def knapsack_with_reps(things, knapsack_weight):
+    things.sort(key=lambda l: l[0] / l[1], reverse=True)  # сортировка по c/w
+
     n = len(things)
     d = [0] * (knapsack_weight + 1)
 
@@ -46,21 +45,18 @@ def knapsack_with_reps(things, knapsack_weight):
 
 # решение задачи о рюкзаке(без повторений вещей)
 def knapsack_without_reps(things, knapsack_weight):
+    things.sort(key=lambda l: l[0] / l[1], reverse=True)  # сортировка по c/w
+
     n = len(things)
-    d = [0] * (knapsack_weight + 1)
+    d = [[0 for _ in range(0, knapsack_weight + 1)]] * n
 
     weight = [i[1] for i in things[:n]]
     price = [i[0] for i in things[:n]]
 
-    # https://stepik.org/lesson/13259/step/3
+    for i in range(1, n):
+        for w in range(0, knapsack_weight + 1):
+            d[i][w] = d[i-1][w]
+            if weight[i] <= w:
+                d[i][w] = max(d[i-1][w], d[i-1][w-weight[i]] + price[i])
 
-
-if __name__ == '__main__':
-    knapsack_w = 10
-    # things = [[60, 20], [100, 50], [120, 30]]  # and [20, 50, 30], [60, 100, 120], 50
-    # things = [[20, 20], [25, 30], [10, 10]]
-    things = [[30, 6], [14, 3], [16, 4], [9, 2]]
-
-    print('knapsack_with_proportion() ==', knapsack_with_proportion(things, knapsack_w))
-    print('knapsack_with_reps() ==', knapsack_with_reps(things, knapsack_w))
-    print('knapsack_without_reps() ==', knapsack_without_reps(things, knapsack_w))
+    return d[-1][-1]

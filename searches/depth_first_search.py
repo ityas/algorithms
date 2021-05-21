cef7f447fb2ebc40d3f66404390799c0f9229690
Seraphim Ryaboshapko <70561906+ityas@users.noreply.github.com>
2021-05-21 14:50:50 +0700

    Update depth_first_search.py

--- searches/depth_first_search.py
+++ searches/depth_first_search.py
@@ -1,7 +1,7 @@
 # graph = { node: (adjacent_node, adjacent_node) }
 
 # поиск в глубину
-def dfs(graph, start_node, visited=None):
+def depth_first_search(graph, start_node, visited=None):
     if visited is None:
         visited = list()
 
@@ -9,6 +9,6 @@ def dfs(graph, start_node, visited=None):
         visited.append(start_node)
 
     for next_node in graph[start_node] - set(visited):
-        dfs(graph, next_node, visited)
+        depth_first_search(graph, next_node, visited)
 
     return visited
